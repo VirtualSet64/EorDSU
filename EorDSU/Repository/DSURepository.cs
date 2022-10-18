d9b750b93196027e2ed262c128466660386c8ae7
programmsit <programmsit@RAMIDIN-PC30>
2022-10-18 12:30:19 +0300

    Add ParsingController

---
+++ EorDSU/Repository/DSURepository.cs
@@ -0,0 +1,33 @@
+﻿using EorDSU.Interface;
+using EorDSU.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace EorDSU.Repository
+{
+    public class DSURepository : IDSURepository
+    {
+        
+
+        public List<Cathedra> GetCathedra()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Department> GetDepartment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Discipline> GetDiscipline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Faculty> GetFaculty()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
