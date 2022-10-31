87f4b19a603cde0421d624381b91841171865f0e
programmsit <programmsit@RAMIDIN-PC30>
2022-10-31 11:53:50 +0300

    Добавил авторизацию

---
+++ EorDSU/Controllers/EorController.cs
@@ -0,0 +1,61 @@
+﻿using EorDSU.ConfigInfo;
+using EorDSU.DBService;
+using EorDSU.Interface;
+using EorDSU.Models;
+using EorDSU.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace EorDSU.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EorController : Controller
+    {
+        private readonly ApplicationContext _context;
+        private readonly BASEPERSONMDFContext _basePersonContext;
+        private readonly AuthOptions _authOptions;
+
+        public EorController(ApplicationContext context, BASEPERSONMDFContext basePersonContext, AuthOptions authOptions)
+        {
+            _context = context;
+            _basePersonContext = basePersonContext;
+            _authOptions = authOptions;
+        }
+
+        [HttpGet]
+        public List<FileModel> GetFiles()
+        {
+            return _context.FileModels.ToList();
+        }
+
+        /// <summary>
+        /// Вход
+        /// </summary>
+        /// <param name="requestUser"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IResult Login(User requestUser)
+        {
+            User? user = _context.Users.FirstOrDefault(p => p.UserName == requestUser.UserName && p.Password == requestUser.Password);
+            // если пользователь не найден, отправляем статусный код 401
+            if (user is null) return Results.Unauthorized();
+
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.UserName) };
+            // создаем JWT-токен
+            var jwt = _authOptions.GetAuthData(claims);
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            // формируем ответ
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = user.UserName
+            };
+
+            return Results.Json(response);
+        }
+    }
+}
--- EorDSU/Models/User.cs
+++ EorDSU/Models/User.cs
@@ -1,8 +1,16 @@
-﻿//using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
-//namespace EorDSU.Models
-//{
-//    public class User : IdentityUser
-//    {
-//    }
-//}
+namespace EorDSU.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; } = null!;
+        [PasswordPropertyText]
+        public string Password { get; set; } = null!;
+    }
+}
